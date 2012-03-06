80b75c5e46081fa81d9eb1d00551c353cbcbc1f8
Firas Abuzaid <fabuzaid21@gmail.com>
2012-03-06 10:43:59 -0800

    Created Dictionary, opens and reads file.

---
+++ src/Dictionary.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+
+public class Dictionary {
+
+	private Map<String, String> dict;
+
+	public Dictionary(String file) {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while (true) {
+				String line = reader.readLine();
+				String[] words = line.split(" ");
+				dict.put(words[0], words[1]);
+
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getWord(String spanishWord) {
+		return dict.get(spanishWord);
+	}
+
+}
--- src/MachineTranslator.java
+++ src/MachineTranslator.java
@@ -1,8 +1,25 @@
-
 public class MachineTranslator {
 
-	
+	private Dictionary dict;
+
 	public static void main(String[] args) {
-		
+		// write rules
+		// open the 10 Spanish sentences, and map each word into English
+		// Run the POS tagger on the English
+		// (Almost there) take rules and apply them to the English words to
+		// reorder them into sentences
+		// print out results!
+
+		MachineTranslator translator = new MachineTranslator(args[0]);
+		translator.run();
+
+	}
+	private MachineTranslator(String file) {
+		// TODO Auto-generated constructor stub
+		dict = new Dictionary(file);
+	}
+
+	public void run() {
+
 	}
 }
