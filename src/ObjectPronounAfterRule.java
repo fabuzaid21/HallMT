181a7a7099ed5496012ca004734954bb80b717b2
Tina Roh <tinaroh@gmail.com>
2012-03-10 13:48:55 -0800

    Done with Readme

--- src/ObjectPronounAfterRule.java
+++ src/ObjectPronounAfterRule.java
@@ -3,6 +3,5 @@ public class ObjectPronounAfterRule extends RegexReorderRule {
 	public ObjectPronounAfterRule() {
 		reorderRegex = "({PRP})([{VERB}{MD}]+)";
 		reorderOrder = new int[] { 2, 1 };
-		// TODO: Check if PRP is a he/she subject pronoun
 	}
 }
